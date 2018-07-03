19d885a469bad58486c24a261e51c7176e9896b0
Tushar Joshi <tusharvjoshi@gmail.com>
2018-07-03 16:42:28 +0530

    09. Simple Value Data

--- finish/src/app/components/user-info/user-info.component.js
+++ finish/src/app/components/user-info/user-info.component.js
@@ -11,11 +11,16 @@
             restrict: "E",
             templateUrl: "/app/components/user-info/user-info.component.html",
             scope: {
-                customer: "="
+                customer: "=",
+                initialCollapsed: "@collapsed"
             },
             controller: ['$scope', function($scope) {
+                $scope.collapsed = ($scope.initialCollapsed === 'true');
                 $scope.upgrade = function(customer) {
                     customer.status = "Upgraded";
+                }
+                $scope.collapse = function() {
+                    $scope.collapsed = !$scope.collapsed;
                 }
             }]
         };
